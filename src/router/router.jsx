645d2254ad4c644f15b643a9e24919e731ec8491
Siti Anisah <113233993+SitiAnisah17@users.noreply.github.com>
2024-05-21 23:21:03 +0700

    Update router.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout.jsx";
 import Home from "../pages/Home.jsx";
 import Services from "../pages/Services.jsx";
+import Services2 from "../pages/Services2.jsx";
 import Portfolio from "../pages/portfolio.jsx";
 import LoginForm from "../pages/login.jsx";
 import SignupForm from "../pages/signup.jsx";
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "/services",
         element: <Services />,
+      },
+       {
+        path: "/services2",
+        element: <Services2 />,
       },
       {
         path: "/portfolio",
